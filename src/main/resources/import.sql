daf1c99af3b08afc9344fc1672a2a417a9d6bb98
Ana Beatriz Thomaz <201908047471@alunos.estacio.br>
2021-04-10 21:37:39 -0300

    3.17. Mapeando relacionamento com @ManyToOne

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
-insert into cozinha (nome) values ('Tailandesa');
-insert into cozinha (nome) values ('Indiana');
+insert into cozinha (id, nome) values (1,'Brasileira');
+insert into cozinha (id, nome) values (2,'Fast-Food');
 
-insert into restaurante (nome, taxa_frete) values ('Tempero da Vovó', 5)
-insert into restaurante (nome, taxa_frete) values ('Burguesa Hamburgueria Artesanal', 4.50)
-insert into restaurante (nome, taxa_frete) values ('SamBar & Restaurante', 7)
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tempero da Vovó', 5, 1)
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Burguesa Hamburgueria Artesanal', 4.50, 2)
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('SamBar & Restaurante', 7, 1)
